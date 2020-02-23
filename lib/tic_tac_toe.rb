4ac44c577c65ec6185aff5baa2f394a7a69bae8b
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-02-23 04:23:13 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -50,4 +50,8 @@ class TicTacToe
   def valid_move?(index)
     index.between?(0, 8) && !position_taken?(index)
   end
+
+  def turn_count
+    @board.count { |token| token == 'X' || token == 'O' }
+  end
 end
