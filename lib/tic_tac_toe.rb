8ef38ff71e9e724f689760cc8c6826e7ec3b30a9
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-02-23 04:19:34 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -34,4 +34,16 @@ class TicTacToe
   def position_taken?(index)
     @board[index] == 'X' || @board[index] == 'O'
   end
+
+  def turn
+    puts 'Please enter 1-9:'
+    user_input = gets.strip
+    index = input_to_index(user_input)
+    if valid_move?(board, index)
+      move(@board, index, current_player(@board))
+      display_board(@board)
+    else
+      turn(@board)
+    end
+  end
 end
