fad83d85c8341c68c7fb3068650bb1f03f590050
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-02-23 04:25:39 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -58,4 +58,12 @@ class TicTacToe
   def current_player
     turn_count.even? ? 'X' : 'O'
   end
+
+  def won?
+    WIN_COMBINATIONS.detect do |combo|
+      board[combo[0]] == board[combo[1]] &&
+        board[combo[1]] == board[combo[2]] &&
+        position_taken?(board, combo[0])
+    end
+  end
 end
